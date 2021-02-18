a90347bfe6bfbbbe2d429859790bf0989ed67be1
Nick Chubb <Nick.r.chubb@gmail.com>
2021-02-18 13:36:35 -0800

    update links

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -8,19 +8,19 @@ const Header = () => {
             <div className="header">
                 <h1>Hawking<small> - The SFU Science Discord Bot</small></h1>
                 <div className="button-container">
-                    <Link to="/">
+                    <Link to="./">
                         <Button color="dodgerblue" text="🤖 Dashboard" />
                     </Link>
-                    <Link to="/newevent">
+                    <Link to="./newevent">
                         <Button color="dodgerblue" text="🎉 Add event to Event Calendar" />
                     </Link>
-                    <Link to="/documentation">
+                    <Link to="./documentation">
                         <Button color="dodgerblue" text="📜 Documentation" />
                     </Link>
                     <a href="https://github.com/NickChubb/science-bot">
                         <Button color="dodgerblue" text="🐙 GitHub" />
                     </a>
-                    <Link to="/settings">
+                    <Link to="./settings">
                         <Button color="dodgerblue" text="⚙️ Settings" />
                     </Link>
                     <Button color="dodgerblue" text="🗄 View Database" disabled={true} />
