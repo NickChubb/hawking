3d803b0380c7c91fe2ce0e28522c5148e2d3bc8a
Nick Chubb <Nick.r.chubb@gmail.com>
2021-02-18 14:09:30 -0800

    Change index path

--- index.js
+++ index.js
@@ -618,7 +618,7 @@ app.post('/bot/updateCalendar', (req,res) => {
     res.sendStatus(200);
 })
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
