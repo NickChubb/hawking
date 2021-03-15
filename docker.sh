d9d1d2371bbaa0d517fbf80b8e3052c9f4bd2227
Nick Chubb <Nick.r.chubb@gmail.com>
2021-03-15 15:29:48 -0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,13 @@
-ln -s config.json src/res/config.json
-docker stop hawking
-docker rm hawking
-docker build -t nchubb/hawking .
-docker run --restart unless-stopped --name hawking -p 3001:8080 -v db:/usr/src/app/data -d nchubb/science-bot
+CONF=config.json
+
+if [ -f "$CONF" ]; then
+    ln -s $CONF src/res/$CONF
+    yarn upgrade
+    docker stop hawking
+    docker rm hawking
+    docker build -t nchubb/hawking .
+    docker run --restart unless-stopped --name hawking -p 3001:8080 -v db:/usr/src/app/data -d nchubb/science-bot
+else
+    echo "ERROR: No $CONF file found => Edit config.json.sample and rename to config.json."
+fi
+
